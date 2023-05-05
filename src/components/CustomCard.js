aff9e0c46b8f5854046db13ad95831645cbdea94
ol100 <javiercalle3@gmail.com>
2023-05-05 21:33:36 +0200

    card experiments

---
+++ src/components/CustomCard.js
@@ -0,0 +1,9 @@
+import cards from '../css/card.module.css'
+
+function CustomCard(){
+    return(<div className={cards.card}>
+    </div>
+    )
+}
+
+export default CustomCard;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,12 @@
 import ActionAreaCard from './Cards';
+import CustomCard from './CustomCard';
 function Section({reference, classname, name}){
     
     return(
         <div ref={reference} className={`${classname} space`}>
         <h3>{name}</h3>
         <ActionAreaCard />
+        <CustomCard />
       </div>
     )
 }
